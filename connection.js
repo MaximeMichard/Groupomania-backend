affa924a5755c55c90939ed3a20851b79f03baf8
Michard Maxime <maxime.michard@laposte.net>
2020-10-14 18:21:57 +0200

    .

--- connection.js
+++ connection.js
@@ -2,8 +2,8 @@ const Sequelize= require ('sequelize');
 require('dotenv').config()
 
 //Connection Base de donnée //
-const sequelize = new Sequelize('z5jp8801aj6yrr8o', 'fl8oyo3052j42tzb', 'aup07hemgzhlmt76', {
-    host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306',
+const sequelize = new Sequelize('groupomania', 'root', '', {
+    host: 'localhost',
     dialect: 'mysql',
     logging: false
   })
--- controllers/post.js
+++ controllers/post.js
@@ -58,24 +58,29 @@ exports.allPost = async (req,res,next) => {
 
 exports.getPostCommentaire= async (req,res,next)=> {
   try{
-    let _get= await models.post.findOne({
+    let _postwithComment= await models.post.findOne({
       where: { id: Number(req.params.id)},
       include: [{
         model: models.commentaire,
-      }]
-    })
-      return res.status (200).json (_get);
-      let _commentaire = await models.commentaire.findAll()
-    return res.status(200).json(_commentaire);
-    /* for (let index = 0; index < _get.length; index++) {
-      let element = _get[index];  
+        
+      }],
+     
+    }) 
+    for (let index = 0; index < _postwithComment.commentaires.length ; index++) {
+      let element = _postwithComment.commentaires[index]; 
+      console.log(element); 
       let _user= await models.User.findOne({
-        where: { id: Number(element.userId) },
+        where: { id: Number(element.UserId) },
         attributes:{ exclude: ['password']} 
     });
     element.user = _user;
-    _get[index] = element ;
-    } */   
+    _postwithComment.commentaires[index] = element ;
+    console.log(_user);
+    }
+    
+    
+    return res.status (200).json (_postwithComment);
+       
      
   }
   catch(err){
