30bb5852cd21ae925857a8c56c943434dfa312dd
Michard Maxime <maxime.michard@laposte.net>
2020-10-20 16:31:32 +0200

    config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
-
+ 
 module.exports = {
 
     "development": {
-      "username": process.env.DB_USERNAMES,
-      "password": process.env.DB_PASSWORDS,
-      "database": process.env.DB_DATABASES,
-      "host": process.env.DB_HOSTS,
-      "dialect": process.env.DB_DIALECTS
+      "username": "fl8oyo3052j42tzb",
+      "password": "aup07hemgzhlmt76",
+      "database": "z5jp8801aj6yrr8o",
+      "host": "un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      "dialect": "mysql"
     },
     "test": {
       "username": process.env.DB_USERNAMES,
