6baea43cf9985698ba91c3897493122aca8ef4fb
Michard Maxime <maxime.michard@laposte.net>
2020-10-20 16:29:25 +0200

    .

--- config/config.js
+++ config/config.js
@@ -3,25 +3,25 @@ require('dotenv').config();
 module.exports = {
 
     "development": {
-      "username": process.env.DB_USERNAME1,
-      "password": process.env.DB_PASSWORD1,
-      "database": process.env.DB_DATABASE1,
-      "host": process.env.DB_HOST1,
-      "dialect": process.env.DB_DIALECT1
+      "username": process.env.DB_USERNAMES,
+      "password": process.env.DB_PASSWORDS,
+      "database": process.env.DB_DATABASES,
+      "host": process.env.DB_HOSTS,
+      "dialect": process.env.DB_DIALECTS
     },
     "test": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_DATABASE,
-      "host": process.env.DB_HOST,
-      "dialect": process.env.DB_DIALECT
+      "username": process.env.DB_USERNAMES,
+      "password": process.env.DB_PASSWORDS,
+      "database": process.env.DB_DATABASES,
+      "host": process.env.DB_HOSTS,
+      "dialect": process.env.DB_DIALECTS
     },
     "production": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_DATABASE,
-      "host": process.env.DB_HOST,
-      "dialect": process.env.DB_DIALECT
+      "username": process.env.DB_USERNAMES,
+      "password": process.env.DB_PASSWORDS,
+      "database": process.env.DB_DATABASES,
+      "host": process.env.DB_HOSTS,
+      "dialect": process.env.DB_DIALECTS
     } 
 }
 
